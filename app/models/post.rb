57a2aaf5fed9728b07349052e744222274a17474
petar2hypnotize <petar@2hypnotize.com>
2015-12-12 19:32:39 +0100

    Added a lot, again.. cool... ^_^

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+  validates :user_id, presence: true
   has_attached_file :image, styles: { :medium => "640x" }
   # Validate content type
   validates_attachment_content_type :image, content_type: /\Aimage/
